d52c5283c0870ec0fcca84dcb1022693d9f01dd3
Alberto Monge <alberto@MacBook-Pro-di-Alberto.local>
2017-05-29 11:19:51 +0200

    dodicesimo laboratorio TdP

---
+++ Lab12_Rivers/src/it/polito/tdp/rivers/db/RiversDAO.java
@@ -0,0 +1,75 @@
+package it.polito.tdp.rivers.db;
+
+import it.polito.tdp.rivers.model.Flow;
+import it.polito.tdp.rivers.model.River;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RiversDAO {
+
+	public List<River> getAllRivers() {
+		final String sql = "SELECT id, name FROM river";
+
+		List<River> rivers = new LinkedList<River>();
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				rivers.add(new River(res.getInt("id"), res.getString("name")));
+			}
+
+			conn.close();
+
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new RuntimeException();
+		}
+
+		return rivers;
+	}
+
+	public List<Flow> getAllFlows(List<River> rivers) {
+		final String sql = "SELECT id, day, flow, river FROM flow";
+
+		List<Flow> flows = new LinkedList<Flow>();
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				flows.add(new Flow(res.getDate("day").toLocalDate(), res.getDouble("flow"),
+						rivers.get(rivers.indexOf(new River(res.getInt("river"))))));
+			}
+
+			conn.close();
+
+		} catch (SQLException e) {
+			// e.printStackTrace();
+			throw new RuntimeException();
+		}
+
+		return flows;
+	}
+
+	public static void main(String[] args) {
+		RiversDAO dao = new RiversDAO();
+
+		List<River> rivers = dao.getAllRivers();
+		System.out.println(rivers);
+
+		List<Flow> flows = dao.getAllFlows(rivers);
+		System.out.format("Loaded %d flows\n", flows.size());
+		// System.out.println(flows) ;
+	}
+
+}
